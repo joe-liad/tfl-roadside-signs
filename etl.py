5b2520240db3b656a60cae2125b280aa31e4bbf5
joe-liad <92937667+joe-liad@users.noreply.github.com>
2022-01-23 23:20:36 +0000

    comment the etl process

--- etl.py
+++ etl.py
@@ -1,3 +1,5 @@
 import pandas as pd
+# extract the signs array only (not the feed metadata) from the xml file
 df = pd.read_xml('./signs.xml', xpath=".//doc:Sign", namespaces= {'doc':"http://www.tfl.gov.uk/vms/1.0"})
+# write the array to pretty printed json (easier to diff than csv rows)
 js = df.to_json('./signs.json', orient='records', indent=4)
